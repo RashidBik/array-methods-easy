3f65a1fe3ed508b036fe96efc73284e672970070
RashidBik <abdurashid.bigzad1999@gmail.com>
2022-10-04 23:35:40 +0430

    Update SliceSyn.jsx
    escape character was added

--- pages/events/SliceSyn.jsx
+++ pages/events/SliceSyn.jsx
@@ -28,7 +28,7 @@ const SliceSyn = () => {
           to convert Array-like objects/collections
           to a new Array. You just bind the method 
           to the object. The arguments inside a function 
-          is an example of an 'array-like object'. 
+          is an example of an array-like object. 
             </code>
         </div>
         <div className='usage'>
